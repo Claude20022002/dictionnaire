a2851bb98eb094869dd313c99aa62eeb3a316305
Claude20022002 <clusamote@gmail.com>
2025-03-09 12:18:18 +0100

    Premier avancement
    Création des chemins

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
-import Loader from "./components/Loarder";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
     return (
         <div>
-            <Loader />
+            <Router>
+                <Routes>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/apropos" element={<Apropos />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return <div>Header</div>;
+    return (
+        <>
+            <Stack component="header" sx={{ width: "100%", padding: "15px" }}>
+                <Stack>
+                    <Typography
+                        variant="h1"
+                        sx={{
+                            fontWeight: "bold",
+                            boxSizing: "border-box",
+                            fontSize: { xs: "1rem", sm: "2rem", md: "3rem" },
+                        }}
+                    >
+                        Dictionnaire
+                    </Typography>
+                    <Stack component="nav">
+                        <ul>
+                            <li>
+                                <Link to="/apropos">À propos</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </Stack>
+                </Stack>
+            </Stack>
+        </>
+    );
 }
